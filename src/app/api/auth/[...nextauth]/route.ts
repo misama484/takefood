d6b6634a59eb13761113185871c50b74a637152a
Miguel Sanchez Martínez <msanchez@inired.com>
2024-07-06 17:50:55 +0200

    cambios en casa

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,14 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+
 const handler = NextAuth({
   providers: [
+
     CredentialsProvider({
 
       name: 'Credentials',
+      id:'credentials',
 
       credentials: {
         username: { label: "Email", type: "email", placeholder: "test@example.com" },
@@ -12,16 +16,7 @@ const handler = NextAuth({
       },
       async authorize(credentials, req) {
 
-        const res = await fetch("/your/endpoint", {
-          method: 'POST',
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" }
-        })
-        const user = await res.json()
-
-        if (res.ok && user) {
-          return user
-        }
+        console.log({credentials});
 
         return null
       }
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,21 +1,54 @@
-import Image from "next/image"
+'use client'
+import Image from "next/image";
+import { useState } from "react";
+import { signIn } from "next-auth/react"
 
 function LoginPage() {
+
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ loginInProgress, setLoginInProgres ] = useState(false);
+
+  async function handleFormSubmit(ev){
+    ev.preventDefaul();
+    setLoginInProgres(true);
+    
+    await signIn('credentials', {email, password});
+
+    setLoginInProgres(false);
+  }
+
+
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl font-semibold">
-        Inicio de sesion
+        Login
       </h1>
-      <form className="block max-w-xs mx-auto">
-        <input type="email" placeholder="email"/>
-        <input type="password" placeholder="password"/>
-        <button type="submit">Login</button>
+      <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input 
+          type="email" 
+          name="email" 
+          placeholder="email" 
+          disabled={loginInProgress}/>
+        <input 
+          type="password" 
+          name="password" 
+          placeholder="password" 
+          disabled={loginInProgress}/>        
+        <button type="submit" disabled={loginInProgress}>Iniciar sesion</button>
+        
         <div className="my-4 text-center text-gray-500">
-          Iniciar con</div>
-          <button className="flex justify-center items-center gap-2 text-lg">
+          O <br/>Iniciar con</div>
+          <button className="flex justify-center items-center gap-2 text-lg" >
           <Image src={'/googleLogo.png'} alt="googleLogo" width={35} height={35}/>
           Google
         </button>
+        <div className="my-4 text-center text-gray-500">
+          No tienes cuenta? <a href="/register" className="underline font-semibold">Registro</a>
+        </div>
+        <div className="my-4 text-center text-gray-500">
+           <a href="/" className="underline font-semibold">Recuperar password</a>
+        </div>
         
       </form>
     </section>
